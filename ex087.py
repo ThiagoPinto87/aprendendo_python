ea99a15b7f49d95b3cc220dec2af6e4a5607e8b9
ThiagoPinto87 <thiagopaescontador@gmail.com>
2022-06-08 21:41:42 -0400

    Resolve o exercício 087 e já faz a análise dos passos a serem executados no exercício 088.

--- ex087.py
+++ ex087.py
@@ -5,6 +5,7 @@ c) O Maior valor digitado na segunda linha."""
 
 
 matriz_numeros = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # Cria a lista para depois fazer a matriz.
+par = mai = coluna = 0  # Instancias zeradas e iniciais.
 for l in range(3):  # Laço de repetição para preencher os índices da "matriz_numeros".
     for c in range(3):  # Armazena 3 números para cada índice da lista dentro da lista da "matriz_numeros".
         matriz_numeros[l][c] = int(input(f'Digite valor para [{l}, {c}]: '))  # Captura os números. Na estrutura já informa o índice que será inserido.
@@ -12,5 +13,22 @@ print('-=' * 20)
 for l in range(3):  # Laço de repetição para que ele imprima cada índice da lista individualmente até terminar a lista.
     for c in range(3):
         print(f'[{matriz_numeros[l][c]:^5}]', end='')  # Centralização do texto utilizando 5 caracteres. ":^5"
+        if matriz_numeros[l][c] % 2 == 0:  # Verifica se cada valor inserido é par.
+            par += matriz_numeros[l][c]  # Insere cada valor verificado como par no laço acima dentro da variável "par"
     print()
 print('-=' * 20)
+print(f'A soma dos valores par é {par}!')
+for l in range(3):  # Laço de repetição de três fases.
+    coluna += matriz_numeros[l][2]  # Insere na variável "coluna" cada valor de "matriz_numeros[l][2]" (ou seja
+                                    # índice 2 da linha), somando-os.
+print(f'A soma da terceira coluna é {coluna}!')  # Imprime o resultado da variável "coluna" acima.
+
+for c in range(3):  # Laço de repetição de três fases.
+    if c == 0:  # Verifica se é a primeira informação de "c"
+        mai = matriz_numeros[1][c]  # insere a informação de "c" na variável "matriz_numeros[1][c]" (ou seja indice
+                                    # 1 da linha.
+    elif matriz_numeros[1][c] > mai:  # caso não seja a primeira informação de "c" e for maior que o valor armazenado
+                                      # na variável "mai" o valor digitado em matriz_numero[1][c] substituirá a infor-
+                                      # mação da variável.
+        mai = matriz_numeros[1][c]  # Substitui o valor da variável "mai" em matriz_numeros[1][c]
+print(f'O maior valor da segunda linha é {mai}!')  # Imprime o valor da variável "mai".
--- ex088.py
+++ ex088.py
@@ -1,3 +1,13 @@
-""" Faça um programa que ajude um jogador da Mega Sena a crial palpites.
+""" Faça um programa que ajude um jogador da Mega Sena a criar palpites.
 O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta."""
+
+# Importar biblioteca random
+
+# Criar variável que sorteará 6 números
+
+# Criar variável que perguntará qual a quantidade de jogos quer criar
+
+# Faz as apostas desejadas pelo usuário
+
+# Imprime o resultado.
