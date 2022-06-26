f14d7b28a6482b0ff9c4bbadedb55e4b0128238d
ThiagoPinto87 <thiagopaescontador@gmail.com>
2022-06-25 20:25:14 -0400

    Cria os exercícios 090 à 095

---
+++ ex091.py
@@ -0,0 +1,3 @@
+""" Crie um programa onde 4 jogadores joguem um dado e tenham resultaros aleatórios.
+Garde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior
+numero no dado."""
---
+++ ex092.py
@@ -0,0 +1,3 @@
+""" Crie um programa que leia: nome, ano de nascimento e carteira de trabalho e cadastros (com idade) em um dicionário.
+Se por acaso a CTPS for diferente de zero, o dicionário receberá também o ano de contrataçaõ e o salário.
+Calcule e acrescente além da idade, com quantos anos a pessoa vai se aposentar."""
