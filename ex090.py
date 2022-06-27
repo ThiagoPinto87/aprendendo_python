3d623b3dbc23b10bcdea9e0e4487ba216e7d7798
ThiagoPinto87 <thiagopaescontador@gmail.com>
2022-06-26 20:06:31 -0400

    Resolvendo os exercícios 90 e 91.

--- ex090.py
+++ ex090.py
@@ -1,2 +1,32 @@
-""" Fala um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+""" Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela."""
+
+
+# Iterações iniciais ou zeradas
+ficha = dict()  # Cria a ficha do aluno.
+media = 0  # Cria a variável media e seta o valor 0.
+
+# Captura as informações do aluno.
+ficha['nome'] = str(input('Nome do aluno: ')).upper().strip()  # Cria a key 'nome' e solicita input.
+ficha['media'] = float(input(f'Média de {ficha["nome"]}: '))  # Cria a key media e solicita input.
+
+# Verifica a situação do aluno.
+if ficha['media'] >= 7:
+    ficha['resultado'] = 'APROVADO'
+elif ficha['media'] <= 5:
+    ficha['resultado'] = 'REPROVADO'
+elif ficha['media'] > 5 and ficha['media'] < 7:  # Essa strig poderia ter sido escrita elif 5 <= ficha['media'] < 7
+    ficha['resultado'] = 'RECUPERAÇÃO'
+
+# Imprime o resultado.
+print('-=' * 15)
+print(f'- O nome do aluno é {ficha["nome"]}.')
+print(f'- A média de {ficha["nome"]} é: {ficha["media"]}')
+print(f'- A situação é {ficha["resultado"]}!')
+
+# Após ver a aula do professor guanabara, vou fazer a solução igual ao dele.
+
+print('-=' * 15)
+print('-=-=-=-FORMA GUANABARA-=-=-=-')
+for k, v in ficha.items():
+    print(f'- {k} é igual a {v}')
--- ex091.py
+++ ex091.py
@@ -1,3 +1,57 @@
-""" Crie um programa onde 4 jogadores joguem um dado e tenham resultaros aleatórios.
-Garde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior
+""" Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+Guarde esses resultados num dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior
 numero no dado."""
+
+# Não consegui ordenar os dicionários. Tive que assistir a resolução do professor guanabara.
+
+"""# Importação de bibliotecas
+from random import randint
+from time import sleep
+
+# Iterações iniciais e zeradas
+tt_jogadas = dict()
+resultado = list()
+cont = 1
+
+# Simula os jogos dos jogadores imprimindo os resultados
+for j in range(4):
+    tt_jogadas['nome'] = 'Jogador' + str(cont)
+    tt_jogadas['jogo'] = randint(1, 6)
+    cont += 1
+    resultado.append(tt_jogadas.copy())
+
+print('VALORES SORTEADOS')
+for r in resultado:
+     print(f'{resultado[r]["nome"]} tirou {resultado[r]["jogo"]} no dado.')
+     sleep(1)
+resultado.sort()"""
+
+# Importação de bibliotecas.
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+print('-=-=-=- JOGO DE DADOS -=-=-=-')
+sleep(2.5)
+# Simula o jogo de cada jogador.
+jogo = {'Jogador 1': randint(1, 6),  # Cria cada key como nome de cada jogador e insere um resultado aleatório de 1 a 6.
+        'Jogador 2': randint(1, 6),
+        'Jogador 3': randint(1, 6),
+        'Jogador 4': randint(1, 6)}
+
+ranking = list()  # Cria um novo dicionário chamado ranking.
+
+# Imprime o resultado dos jogos
+for k, v in jogo.items():  # Laço de repetição buscando cada item do dicionário jogo.
+    print(f'O {k} tirou {v} no dado.')  # Imprime cada Key "k" e Valor "v" na f-string criada.
+    sleep(1)  # insere um tempo de espera de 1 segundo ao final de cada laço de repetição.
+
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+
+sleep(0.5)
+print('-=-=-=-=- RANKING -=-=-=-=-=-')
+sleep(2)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.')
+    sleep(0.5)
+print('-=' * 14)
