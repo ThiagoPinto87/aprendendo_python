0c7a65b79c6c751d9d88c169cc906e3429e066ec
ThiagoPinto87 <thiagopaescontador@gmail.com>
2022-07-25 23:04:25 -0400

    Resolução do exercício 99, neste exercício não fiz sozinho, pois não consegui entender bem o exercício proposto. Porém, após compreender (e óbvio olhar a resposta), consegui fazer sozinho.

--- ex099b.py
+++ ex099b.py
@@ -3,27 +3,26 @@ Seu programa tem que analisar todos os valores e dizer qual deles é o maior."""
 
 from time import sleep
 
-def maior(* num):
+
+def maior(*num):
     cont = maior = 0
-    print('-=' * 25)
-    print('Analisando os numeros passados...')
-    sleep(0.7)
     for n in num:
-        print(f'{n} ', end='')
-        sleep(0.3)
         if cont == 0:
             maior = n
         elif n > maior:
             maior = n
         cont += 1
-    sleep(0.5)
-    print(f'\nForam informado {cont} valores ao todo.')
-    print(f'O maior valor informado foi o {maior}')
+    print('-=' * 15)
+    print('Analisando os números...')
+    print(num)
+    print(f'Foram gerados {cont} numeros.')
+    print(f'O maior número é {maior}')
 
 
 # Programa principal
-maior(1, 3, 5, 8, 9, 0)
-maior(7, 2, 3, 6)
-maior(8, 5)
+maior(5, 2, 3, 4, 7, 1)
+maior(1, 3, 9, 4, 6)
+maior(0, 1, 4)
+maior(9, 6)
 maior(2)
 maior()
