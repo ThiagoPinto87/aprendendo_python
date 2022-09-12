d02b5a31ffe3399f5965e78c0648c8e47545badd
ThiagoPinto87 <thiagopaescontador@gmail.com>
2022-09-11 20:29:38 -0400

    Conclusão do curso de Python.

---
+++ ex115/lib/arquivo/__init__.py
@@ -0,0 +1,56 @@
+from ex115.lib.interface import *
+
+
+def arquivo_bd_existe(nome_arquivo):  # A idéia dessa função é retornar como verdadeiro ou falso.
+    try:
+        abrir = open(nome_arquivo, 'rt')  # Verifica se o arquivo existe. O "rt" quer dizer leia texto, com isso,
+        abrir.close()
+        # ele acaba verificando se o arquivo existe.
+    except FileNotFoundError:
+        return False
+    else:
+        print('Sistema carregado corretamente.')
+        return True
+
+
+def criar_arquivo(nome_arquivo):
+    try:
+        criando = open(nome_arquivo, 'wt+')  # O parâmetro "wt" quer dizer escreva texto, o "+" quer dizer crie caso
+        criando.write('NOME;IDADE\n')
+        # não tenha.
+        criando.close()
+    except:
+        print('Não foi possível criar o banco de dados.')
+        print('Favor contactar o desenvolvedor.')
+    else:
+        print(f'Arquivo {nome_arquivo} criado com sucesso.')
+
+
+def ler_arquivo(nome_arquivo):
+    try:
+        lendo = open(nome_arquivo, 'rt')
+    except:
+        print('Erro ao ler o arquivo.')
+        print('Contacte o suporte.')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        for i in lendo:
+            lançamento = i.split(';')
+            lançamento[1] = lançamento[1].replace('\n', '')
+            print(f'{lançamento[0]:<30}{lançamento[1]:>3}')
+    finally:
+        lendo.close()
+
+
+def cadastrar(nome_arquivo, nome='DESCONHECIDO', idade=0):
+    try:
+        cadastrando = open(nome_arquivo, 'at')
+    except:
+        print('ERRO ao abrir o arquivo ao cadastrar pessoas.')
+    else:
+        try:
+            cadastrando.write(f'{nome};{idade}\n')
+        except:
+            print('Erro! Não foi possivel cadastrar e pessoa.')
+        else:
+            print(f'{nome} cadastrado com sucesso.')
--- ex115/siscadpessoas.py
+++ ex115/siscadpessoas.py
@@ -1,17 +1,29 @@
+from ex115.lib.arquivo import *
 from ex115.lib.interface import *
 from time import sleep
 
+
+# Cria o arquivo de texto que servirá como "banco de dados"
+banco_dados = 'banco_dados.txt'
+
+if not arquivo_bd_existe(banco_dados):  # Verifica se o arquivo está criado e caso não esteja, ele o criará.
+    criar_arquivo(banco_dados)
+
+
+# Menu do sistema
 while True:
     resposta = menu(['Listar Pessoas', 'Cadastrar Pessoas', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1 selecionada')
+        # Opção de listar o conteúdo do arquivo.
+        ler_arquivo(banco_dados)
     elif resposta == 2:
-        cabeçalho('Opção 2 selecionada')
+        cabeçalho('CADASTRAR PESSOAS')
+        nome = str(input('Nome: ')).upper()
+        idade = leia_int('Idade: ')
+        cadastrar(banco_dados, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do programa... Até Logo!')
         break
     else:
         print('\33[1;31mERRO! por favor, digite uma opção válida.\33[m')
-    sleep(2.5)
-
-
+    sleep(1.5)
